4255356a40541a53322ef820c4930b403a27d0d5
Nanda Mochammad <nanda.rosyadi@noble-software.com>
2025-07-28 14:44:07 +0700

    Init Project, add Asset, Add Data Source https://www.youtube.com/watch?v=11iFAWFO0AU

---
+++ TravelMap/Applications/Models/LocationModel.swift
@@ -0,0 +1,18 @@
+//
+//  LocationModel.swift
+//  TravelMap
+//
+//  Created by Nanda Mochammad on 28/07/25.
+//
+
+import Foundation
+import MapKit
+
+struct LocationModel {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
