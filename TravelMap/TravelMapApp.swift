717b06a26a363f4502e255307e7a6a9553286b37
Nanda Mochammad <nanda.rosyadi@noble-software.com>
2025-07-28 14:28:06 +0700

    Initial Commit

---
+++ TravelMap/TravelMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMapApp.swift
+//  TravelMap
+//
+//  Created by Nanda Mochammad on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
